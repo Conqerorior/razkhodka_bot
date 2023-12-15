48ab2bad31ba7c3fda4a0c548b2baa574996eccc
Серей Овинцев <ozonti@mail.ru>
2023-12-15 17:08:17 +0300

    Добавлены докстринги в БД

--- MongoData.py
+++ MongoData.py
@@ -1,7 +1,9 @@
 import logging
 import os
+from typing import Any, Optional, List, Dict
 
 import motor.motor_asyncio
+from pymongo.results import DeleteResult
 
 client = motor.motor_asyncio.AsyncIOMotorClient(os.getenv('MONGODB'))
 collection = client.razkhodka_db.razkhodka_users
@@ -11,7 +13,8 @@ async def start_mongodb():
     logging.warning('Запуск Базы Данных')
 
 
-async def create_user(state, user):
+async def create_user(state: Any, user: Any) -> None:
+    """Создает нового пользователя в базе данных."""
     async with state.proxy() as data:
         load_data = {
             "user_id": user.id,
@@ -23,17 +26,20 @@ async def create_user(state, user):
     await collection.insert_one(load_data)
 
 
-async def get_user(user):
+async def get_user(user: Any) -> Dict:
+    """Получает данные о пользователе из базы данных."""
     return await collection.find_one({"user_id": user.id})
 
 
-async def get_all_users():
+async def get_all_users() -> List[Dict[str, Any]]:
+    """Получает список всех пользователей из базы данных."""
     cursor = collection.find()
     all_users = await cursor.to_list(length=None)
     return all_users
 
 
-async def show_user(user):
+async def show_user(user: Any) -> Optional[List[Dict[str, Any]]]:
+    """Возвращает список информации о пользователе из базы данных."""
     cursor = collection.find({"user_id": user.id})
     user_data = []
     async for document in cursor:
@@ -42,7 +48,8 @@ async def show_user(user):
     return user_data
 
 
-async def delete_user(user):
+async def delete_user(user: Any) -> Optional[DeleteResult]:
+    """Удаляет пользователя из базы данных."""
     result = await collection.delete_one({'user_id': user.id})
 
     return result
